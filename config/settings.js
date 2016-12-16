8fa0e35d4f7fabd314a6ff9e574f4854c17da3b4
BoringDays <everflashdance@gmail.com>
2016-12-16 18:17:11 +0800

    add proxy, but front side is still unavailable

--- config/settings.js
+++ config/settings.js
@@ -1,4 +1,20 @@
 var path = require('path');
+//var os = require('os');
+//var ipTable = {};
+//var ifaces = os.networkInterfaces();
+
+//for (var dev in ifaces) {
+//  ifaces[dev].forEach(function (details, alias) {
+//    if (details.family == 'IPv4') {
+//      ipTable[dev + (alias ? ':' + alias : '')] = details.address;
+//    }
+//  });
+//}
+
+//ipTable:
+//{ '无线网络连接 5:1': '192.168.191.1',
+//  '本地连接:1': '192.168.16.78',
+//  'Loopback Pseudo-Interface 1:1': '127.0.0.1' }
 
 var port = 8080;
 var srcPath = './src/';
@@ -18,6 +34,12 @@ module.exports = {
     port: port,
     publicPath: outputPath,
     noInfo: false,
-    stats: { colors: true }
+    stats: {colors: true},
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000', // 需要连接的地址，目前服务器用本机IP+3000端口
+        secure: false
+      }
+    }
   }
 };
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,34 +1,20 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var settings = require('./config/settings');
 var path = require('path');
 
-// 直接访问localhost:(port)会出问题，应该访问localhost:(port)/webpack-dev-server
-
-// $ webpack： 生成目标文件
-// $ webpack -p：生成目标文件，并混淆压缩
-// $ webpack -d：生成目标文件，带有source map
-// $ webpack --watch：生成目标文件，并监听
-// http://webpack.github.io/docs/cli.html
-
 module.exports = {
   entry: {
-    app: [
-      // 'webpack/hot/dev-server',
-      'webpack-dev-server/client?http://localhost:' + settings.port, // 如果用到后端服务器的话，可以直接改这里的IP和端口；命令行里面--host 127.0.0.0 --port 1024 可以改端口和IP
-      'webpack/hot/only-dev-server', // "only" prevents reload on syntax errors
-      path.resolve(__dirname, 'src/index.js')
-    ]
+    app: path.resolve(__dirname, 'src/index.js'),
+    vendors: ['react', 'react-dom', 'react-router', 'material-ui', 'immutable']
   },
-  // debug: true,
-  // devtool: 'eval-source-map', // faster in development
-  // devServer: settings.serverSetting,
   devtool: 'source-map',
   output: {
     // path: path.resolve(__dirname, 'dist'), // 输出路径
     path: settings.outputPath,
     publicPath: '/', // 启用服务器（webpack-dev-server/react-hot-loader）的路径，也是HtmlWebpackPlugin里面，js的引用路径；注意后面的/
-    filename: 'app.js'
+    filename: 'js/app.js'
   },
   resolve: {
     extensions: ['', '.js', '.jsx']
@@ -37,11 +23,15 @@ module.exports = {
     loaders: [ // todo 为什么有时候要改成loader才能用？
       {
         test: /\.(sass|scss)$/,
-        loader: 'style!css!sass?sourceMap'
+        // loader: 'style!css!sass?sourceMap'
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css!sass?sourceMap')
       },
       {
         test: /\.css$/,
-        loader: 'style!css'
+        // loader: 'style!css'
+        exclude: /node_modules/,
+        loader: ExtractTextPlugin.extract('style', 'css')
       },
       {
         test: /\.hbs$/,
@@ -65,13 +55,29 @@ module.exports = {
     ]
   },
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
+    // 设置环境变量
+    // 等效于npm scripts 的 set NODE_ENV=production && ... ？
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': 'prod'
+    }),
     new webpack.NoErrorsPlugin(),
+    new ExtractTextPlugin('css/app.css', {
+      allChunks: true
+    }),
     new HtmlWebpackPlugin({
       title: 'Webpack Demo',
       // template: path.join(__dirname, settings.sourcePath, 'index.hbs'),
       template: './src/index.hbs',
-      filename: 'index.html'
+      filename: 'index.html',
+      inject: true,
+      chunksSortMode: 'dependency'
+    }),
+    new webpack.optimize.CommonsChunkPlugin('vendors', 'js/common.js'),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false // Suppress uglification warnings
+      }
     })
   ]
 };
+
