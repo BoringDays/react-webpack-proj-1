50d77171633bb19f4f7bf2925d3007f33049092a
BoringDays <everflashdance@gmail.com>
2016-12-19 15:18:37 +0800

    still not available, asking someone to help

--- config/settings.js
+++ config/settings.js
@@ -1,24 +1,9 @@
 var path = require('path');
-//var os = require('os');
-//var ipTable = {};
-//var ifaces = os.networkInterfaces();
-
-//for (var dev in ifaces) {
-//  ifaces[dev].forEach(function (details, alias) {
-//    if (details.family == 'IPv4') {
-//      ipTable[dev + (alias ? ':' + alias : '')] = details.address;
-//    }
-//  });
-//}
-
-//ipTable:
-//{ '无线网络连接 5:1': '192.168.191.1',
-//  '本地连接:1': '192.168.16.78',
-//  'Loopback Pseudo-Interface 1:1': '127.0.0.1' }
 
 var port = 8080;
-var srcPath = './src/';
-var outputPath = './dist/';
+var srcPath = '../src/';
+var outputPath = '../dist/';
+var publicPath = './public/';
 // var outputPath = path.join(__dirname, '../dist/');
 
 module.exports = {
@@ -27,7 +12,7 @@ module.exports = {
   sourcePath: srcPath,
   outputPath: outputPath,
   serverSetting: {
-    contentBase: srcPath,
+    contentBase: outputPath,
     inline: true,
     hot: true,
     historyApiFallback: true, // 官网说设置为true有利于html5路由的使用
